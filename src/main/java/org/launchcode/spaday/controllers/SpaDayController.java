821ab1c0609e88c51c81c715ed1ee9f8f3dde548
Hank DeDona <icon5585@gmail.com>
2020-02-10 20:09:09 -0600

    Updated code

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -1,6 +1,7 @@
 package org.launchcode.spaday.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -72,7 +73,7 @@ public class SpaDayController {
     @PostMapping(value="")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
-        ArrayList<String> facials = new ArrayList<String>();
+        List<String> facials = new ArrayList<String>();
         facials.add("Microdermabrasion");
         facials.add("Hydrofacial");
         facials.add("Rejuvenating");
@@ -88,7 +89,7 @@ public class SpaDayController {
         model.addAttribute("name", name);
         model.addAttribute("skintype", skintype);
         model.addAttribute("manipedi", manipedi);
-        model.addAttribute("facials", facials);
+        model.addAttribute("appropriateFacials", appropriateFacials);
         
         return "menu";
     }
